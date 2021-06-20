e6e58d11122bc4547684262d4c7ec2a02f064fdf
Ghofrane-thairi <ghofrane.thairi@esprit.tn>
2021-06-20 20:12:39 +0100

    animal & type api

--- src/Entity/Type.php
+++ src/Entity/Type.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\TypeRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=TypeRepository::class)
  */
 class Type
