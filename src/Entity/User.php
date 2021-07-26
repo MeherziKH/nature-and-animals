900592111a56f9671d2524c8b1bdfa6a9ab9f80d
MoezBar <moez.barkallah@esprit.tn>
2021-07-26 11:04:12 +0100

    Authentication backend

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,27 +2,31 @@
 
 namespace App\Entity;
 
-use App\Repository\MembreRepository;
+use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
-use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Serializer\Annotation\Groups;
-
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ApiResource(normalizationContext= {"groups" = {"read"}})
- * @ORM\Entity(repositoryClass=MembreRepository::class)
+ * @ApiResource(formats={"json"})
+ * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class Membre
+class User implements UserInterface
 {
     /**
      * @ORM\Id
-     * @Groups("read")
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
 
     /**
      * @Groups("read")
@@ -40,7 +44,18 @@ class Membre
      * @Groups("read")
      * @ORM\Column(type="string", length=255)
      */
-    private $login;
+    private $tel;
+
+    /**
+     * @Groups("read")
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
 
     /**
      * @var string The hashed password
@@ -49,6 +64,21 @@ class Membre
     private $password;
 
     private $plainPassword;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Publication::class, mappedBy="membre")
+     */
+    private $idPublication;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Order::class, mappedBy="membre")
+     */
+    private $ordres;
+
+    public function __construct()
+    {
+        $this->ordres = new ArrayCollection();
+    }
 
     /**
      * @return mixed
@@ -66,75 +96,51 @@ class Membre
         $this->plainPassword = $plainPassword;
     }
 
-    /**
-     * @Groups("read")
-     * @ORM\Column(type="string", length=255)
-     */
-    private $tel;
-
-    /**
-     * @Groups("read")
-     * @ORM\Column(type="string", length=255)
-     */
-    private $email;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Publication::class, mappedBy="membre")
-     */
-    private $idPublication;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Order::class, mappedBy="membre")
-     */
-    private $ordres;
-
-    public function __construct()
-    {
-        $this->ordres = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getNom(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->nom;
+        return (string) $this->username;
     }
 
-    public function setNom(string $nom): self
+    public function setUsername(string $username): self
     {
-        $this->nom = $nom;
+        $this->username = $username;
 
         return $this;
     }
 
-    public function getPrenom(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
-        return $this->prenom;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
-    public function setPrenom(string $prenom): self
+    public function setRoles(array $roles): self
     {
-        $this->prenom = $prenom;
+        $this->roles = $roles;
 
         return $this;
     }
 
-    public function getLogin(): ?string
-    {
-        return $this->login;
-    }
-
-    public function setLogin(string $login): self
-    {
-        $this->login = $login;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -142,30 +148,6 @@ class Membre
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
-        return $this;
-    }
-
-    public function getTel(): ?string
-    {
-        return $this->tel;
-    }
-
-    public function setTel(string $tel): self
-    {
-        $this->tel = $tel;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
 
         return $this;
     }
@@ -198,6 +180,70 @@ class Membre
         }
 
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * @param mixed $tel
+     */
+    public function setTel($tel): void
+    {
+        $this->tel = $tel;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
     }
 
     /**
@@ -249,6 +295,4 @@ class Membre
         // If you store any temporary, sensitive data on the user, clear it here
         $this->plainPassword = null;
     }
-
-
 }
